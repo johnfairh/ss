f4bac990471be6725652f945ef0a73f0b0167010
John Fairhurst <johnfairh@gmail.com>
2021-05-06 12:11:49 +0100

    Add sourcemap debug helper

--- Package.swift
+++ Package.swift
@@ -87,6 +87,6 @@ let package = Package(
         ]),
       .testTarget(
         name: "LibSassTests",
-        dependencies: ["LibSass", "TestHelpers"]),
+        dependencies: ["LibSass", "TestHelpers", "SourceMapper"]),
     ]
 )
